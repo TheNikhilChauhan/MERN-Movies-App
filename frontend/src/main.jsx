a9c8950629a6f8bad8604ab788e530ef7aae1fed
Nikhil <ace.nikchauhan@gmail.com>
2024-04-25 14:04:19 +0530

    setup of redux and RTK and others

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,5 +1,26 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import store from "./redux/store.js";
+import { Provider } from "react-redux";
+import { Route, RouterProvider, createRoutesFromElements } from "react-router";
+import { createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+// Auth
+
+// Restricted
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<Home />} />
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
+);
