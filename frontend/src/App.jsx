37a9bab7893c42371c00f052b784eb612db489b1
Nikhil <ace.nikchauhan@gmail.com>
2024-04-25 16:28:24 +0530

    added navigation page and some changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,18 @@
+import { Outlet } from "react-router";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Navigation from "./pages/Auth/Navigation";
+
 const App = () => {
-  return <div className=" text-5xl font-bold text-teal-200">App</div>;
+  return (
+    <>
+      <ToastContainer />
+      <main className="p-3">
+        <Navigation />
+        <Outlet />
+      </main>
+    </>
+  );
 };
 
 export default App;
